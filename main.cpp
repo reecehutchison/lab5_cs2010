ef041b59fb081ff6f0e31cad6e51e0589521b0f2
reecehutchison <hutchisora@icloud.com>
2024-11-10 11:14:25 -0700

    did bunch of stuff

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-template<class T>
+template <class T>
 using ELEM=vector<T>;
 
-template<class T>
+template <class T>
 using VEC=vector<ELEM<T>>;
 
 template <class T>
@@ -17,20 +18,68 @@ using pred_t = bool (*) (T);
 template <class T>
 using map_t = T (*) (T,T);
 
+
 template <class T>
-void printElem(ELEM<T> &v) {
-    size_t n=v.size();
-    for(size_t i=0; i<n; ++i) {
-        cout << v[i] << " ";
+void printElem(const ELEM<T>& elems) {
+    int n=elems.size(); 
+    if(n<2) {
+        for(int i=0; i<n; ++i) {
+            cout << elems[i];
+        }
+    } else {
+        cout << "(";
+        for(int i=0; i<n; ++i) {
+            cout << elems[i];
+            if(i!=n-1)
+                cout << " ";
+        }
+        cout << ")";
     }
+}
+
+template <class T>
+void initVec(VEC<T>& my_vector, const ELEM<T>& elements) {
+    for(const T& e : elements) {
+        ELEM<int> curr{e};
+        my_vector.push_back(curr);
+    }    
+}
+
+template <class T>
+void printVec(const VEC<T>& my_vector) {
+    cout << "[ ";
+    int n=my_vector.size(); 
+    for(int i=0; i<n; ++i) {
+        printElem(my_vector[i]);
+        if(i!=n-1)
+            cout << ",";
+        cout << " ";
+    }
+    cout << "]" << endl;
+}
+
+// note: that the intended use is for vectors of the
+//       same size.
+template<class T>
+VEC<T> zip(VEC<T> vec1, VEC<T> vec2) {
+    VEC<T> ans;
+    int n=vec1.size();
+    for(int i=0; i<n; ++i) {
+        ELEM<T> curr;
+        int m=vec1[i].size();
+        for(int j=0; j<m; ++j) {
+            curr.push_back(vec1[i][j]);
+        }
+        for(int j=0; j<m; ++j) {
+            curr.push_back(vec2[i][j]);
+        }
+        ans.push_back(curr);
+    } 
+    return ans;
 }
 
 int main() {
 
-    ELEM<int> a={1, 2, 3};
-
-    printElem(a);
-    cout << endl;
 
     return 0;
 }
